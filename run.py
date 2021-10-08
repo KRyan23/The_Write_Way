97336dfd70ed5c18b998b2db98d15b3936c44670
KRyan23 <GitHub@kryan23.bulc.club>
2021-10-08 15:49:51 +0000

    tested functionality in  run.py

--- run.py
+++ run.py
@@ -5,3 +5,14 @@ if os.path.exists("env.py"):
 
 
 myvar = Flask(__name__)
+
+
+@myvar.route("/")
+def this_is_a_test_function():
+    return "This means our test function in python is working"
+
+
+if __name__ == "__main__":
+    myvar.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
