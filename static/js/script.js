1334262a8290967ce7df6798601981ca3b689408
KRyan23 <GitHub@kryan23.bulc.club>
2021-10-16 20:10:28 +0000

    Fixed main heading layout

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+/* For Carousel */
+document.addEventListener('DOMContentLoaded', function() {
+  var options;
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
