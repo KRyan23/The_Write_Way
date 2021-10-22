e625935c95449ab62553dd0dd91dc9046682dcd0
KRyan23 <GitHub@kryan23.bulc.club>
2021-10-22 15:33:21 +0000

    finished header styling

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
-/* For Carousel */
-document.addEventListener('DOMContentLoaded', function() {
-  var options;
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, options);
-});
+/* */
