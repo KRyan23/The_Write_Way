44fdb71b137c7387e592c1e44bcdaa9949092708
KRyan23 <GitHub@kryan23.bulc.club>
2021-12-16 16:25:25 +0000

    added genre templates

--- app.py
+++ app.py
@@ -31,10 +31,49 @@ def get_genre():
 
 @myvar.route("/policy")
 
-
 def policy():
     policy = mongo.db.policy.find()
     return render_template("policy.html", policy=policy)
+
+
+@myvar.route("/crime")
+
+def crime():
+    crime = mongo.db.crime.find()
+    return render_template("crime.html", crime=crime, title="Crime")
+
+@myvar.route("/fantasy")
+
+def fantasy():
+    fantasy = mongo.db.fantasy.find()
+    return render_template("fantasy.html", fantasy=fantasy, title="Fantasy")
+
+
+@myvar.route("/fiction")
+    
+def fiction():    
+    fiction = mongo.db.fiction.find()
+    return render_template("fiction.html", fiction=fiction, title="Fiction")
+
+
+@myvar.route("/history")
+
+def history():
+    history = mongo.db.history.find()
+    return render_template("history.html", history=history, title="History")
+
+
+@myvar.route("/horror")
+
+def horror():
+    horror = mongo.db.horror.find()
+    return render_template("horror.html", horror=horror, title="Horror")
+
+@myvar.route("/thriller")
+
+def thriller():
+    thriller = mongo.db.thriller.find()
+    return render_template("thriller.html", thriller=thriller, title="Thriller")
 
 
 if __name__ == "__main__":
